4d2fb25653a039b054925efe50b1ae2248ba72a2
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2026-01-03 16:04:09 +0600

    cartup done

--- cartup/lib/widgets/newItem.dart
+++ cartup/lib/widgets/newItem.dart
@@ -1,5 +1,7 @@
+import 'package:cartup/models/groceryItem.dart';
 import 'package:flutter/material.dart';
 import 'package:cartup/data/categories.dart';
+import 'package:cartup/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -9,6 +11,25 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _keyForm = GlobalKey<FormState>();
+  var _itemname = '';
+  var _itemQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _save() {
+    if (_keyForm.currentState!.validate()) {
+      _keyForm.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          category: _selectedCategory,
+          id: DateTime.now.toString(),
+          name: _itemname,
+          quantity: _itemQuantity.toDouble(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,25 +37,49 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Form(
+          key: _keyForm,
           child: Column(
             children: [
               TextFormField(
+                initialValue: _itemname,
                 decoration: InputDecoration(labelText: 'Item Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length > 50) {
+                    return 'Please enter a valid name (1-50 characters).';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _itemname = value!;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
-                      initialValue: '1',
+                      initialValue: _itemQuantity.toString(),
                       decoration: InputDecoration(label: Text('Quantity')),
                       keyboardType: TextInputType.number,
                       keyboardAppearance: Brightness.dark,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.parse(value) <= 0) {
+                          return 'Please enter a valid quantity.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _itemQuantity = int.tryParse(value!)!;
+                      },
                     ),
                   ),
                   SizedBox(width: 10),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
@@ -52,9 +97,30 @@ class _NewItemState extends State<NewItem> {
                             ),
                           ),
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        _selectedCategory = value!;
+                      },
                     ),
                   ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _keyForm.currentState!.reset();
+                    },
+                    child: Text('Reset'),
+                  ), //reset button
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      _save();
+                    },
+                    child: Text("Add Item"),
+                  ), //add item button
                 ],
               ),
             ],
--- cartup/lib/widgets/yourgroceries.dart
+++ cartup/lib/widgets/yourgroceries.dart
@@ -1,6 +1,6 @@
+import 'package:cartup/models/groceryItem.dart';
 import 'package:cartup/widgets/newItem.dart';
 import 'package:flutter/material.dart';
-import 'package:cartup/data/dummydata.dart';
 
 class YourGroceries extends StatefulWidget {
   const YourGroceries({super.key});
@@ -10,12 +10,62 @@ class YourGroceries extends StatefulWidget {
 }
 
 class _YourGroceriesState extends State<YourGroceries> {
+  final List<GroceryItem> _groceryItems = [];
   @override
   Widget build(BuildContext context) {
-    void _addNewItem() {
-      Navigator.of(
+    void _addNewItem() async {
+      final newItem = await Navigator.of(
         context,
       ).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+
+      if (newItem != null) {
+        setState(() {
+          _groceryItems.add(newItem);
+        });
+      }
+    }
+
+    Widget _showItem() {
+      if (_groceryItems.isEmpty) {
+        return Center(child: Text('No items added yet!'));
+      } else {
+        return Container(
+          child: ListView.builder(
+            itemCount: _groceryItems.length,
+            itemBuilder: (ctx, index) => Dismissible(
+              background: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.red,
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                padding: EdgeInsets.only(right: 20),
+                alignment: Alignment.centerRight,
+                child: Icon(Icons.delete, color: Colors.white, size: 40),
+              ),
+              direction: DismissDirection.endToStart,
+              key: ValueKey(_groceryItems[index].id),
+              onDismissed: (ctx) {
+                setState(() {
+                  _groceryItems.removeAt(index);
+                });
+              },
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  height: 24,
+                  width: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text(
+                  '${_groceryItems[index].quantity}',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        );
+      }
     }
 
     return Scaffold(
@@ -23,20 +73,7 @@ class _YourGroceriesState extends State<YourGroceries> {
         title: Text('CartUp'),
         actions: [IconButton(onPressed: _addNewItem, icon: Icon(Icons.add))],
       ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              height: 24,
-              width: 24,
-              color: groceryItems[index].category.color,
-            ),
-            trailing: Text('${groceryItems[index].quantity}'),
-          ),
-        ),
-      ),
+      body: _showItem(),
     );
   }
 }
